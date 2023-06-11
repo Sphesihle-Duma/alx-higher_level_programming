460a9d4127d0f2ba88d7e719f179064341d13a8f
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-06-11 06:04:17 -0700

    Added 3-print_reversed_list_integer.py

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    length = len(my_list)
+    last_element_idx = -1
+    while length > 0:
+        print("{:d}".format(my_list[last_element_idx]))
+        length = length - 1
+        last_element_idx = last_element_idx - 1
+
