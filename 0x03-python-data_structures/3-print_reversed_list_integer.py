c63df486506d190f9866544d831d9ad7b600ad3e
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-06-11 06:52:08 -0700

    fixing empty list issue

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list)
-    last_element_idx = -1
-    while length > 0:
-        if length <= len(my_list):
+    if my_list:
+        length = len(my_list)
+        last_element_idx = -1
+        while length > 0:
             print("{:d}".format(my_list[last_element_idx]))
-        length = length - 1
-        last_element_idx = last_element_idx - 1
+            length = length - 1
+            last_element_idx = last_element_idx - 1
