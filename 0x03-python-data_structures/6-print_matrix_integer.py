0d0e0d3037859c2038fe37285d8a5e83f70f949b
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-06-11 08:02:46 -0700

    Added 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for row in matrix:
+            if row:
+                for num in row:
+                    print("{:d}".format(num), end=" ")
+                print()
