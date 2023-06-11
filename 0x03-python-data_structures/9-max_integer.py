5111451a63553fbe0f90664e2ecf6a98c3ed4311
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-06-11 11:19:46 -0700

    Added 9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    largest = None
+    for i in range(len(my_list)):
+        if largest == None and i == 0:
+            largest = my_list[i]
+        else:
+            if my_list[i] > largest:
+                largest = my_list[i]
+    return largest
