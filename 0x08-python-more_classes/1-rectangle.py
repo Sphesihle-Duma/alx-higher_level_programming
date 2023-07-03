259ba8ab551187b7a56d98a69661322fc4896377
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-07-03 06:15:09 -0700

    handling width and height on the constructor

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,6 +13,14 @@ class Rectangle:
             TypeError: if height or width not int
             ValueError: if height or width < 0
         """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
