9bbbb8dbcd45b3e585cd8a7cd707b3985f7aa95a
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-07-17 16:03:40 -0700

    Added dictionary representation in base module

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,3 +18,16 @@ class Base:
         else:
             Base.__nb_objects += 1
             Base.id = Base.__nb_objects
+
+     def to_dictionary(self):
+        """ Returns a dictionary with attributes """
+        list_atr = ['id', 'size', 'x', 'y']
+        dict_res = {}
+
+        for key in list_atr:
+            if key == 'size':
+                dict_res[key] = getattr(self, 'width')
+            else:
+                dict_res[key] = getattr(self, key)
+
+        return dict_res
