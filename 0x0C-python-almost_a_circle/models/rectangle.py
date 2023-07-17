7710dffad4985f42a9d7c501e32f42826c75a49a
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-07-17 11:41:32 -0700

    validating inputs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,9 +24,9 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """setter method for width"""
-        if not isinstance(width, int):
+        if type(width) is not int:
             raise TypeError("width must be an integer")
-        elif width < 0 or width == 0:
+        elif width <= 0:
             raise ValueError("width must be > 0")
         else:
             self.__width = width
@@ -39,9 +39,9 @@ class Rectangle(Base):
     @height.setter
     def height(self, height):
         """setter method for height"""
-        if not isinstance(height, int):
+        if type(height) is not int:
             raise TypeError("height must be an integer")
-        elif height < 0 or height == 0:
+        elif height <= 0:
             raise ValueError("height must be > 0")
         else:
             self.__height = height
@@ -54,9 +54,9 @@ class Rectangle(Base):
     @x.setter
     def x(self, x):
         """setter method for x"""
-        if not isinstance(x, int):
+        if type(x) is not int:
             raise TypeError("x must be an integer")
-        elif x < 0 or x == 0:
+        elif x < 0:
             raise ValueError("x must be > 0")
         else:
             self.__x = x
@@ -69,9 +69,9 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         """setter method for y"""
-        if not isinstance(y, int):
+        if type(y) is not int:
             raise TypeError("width must be an integer")
-        elif y < 0 or y == 0:
+        elif y < 0:
             raise ValueError("width must be > 0")
         else:
             self.__y = y
