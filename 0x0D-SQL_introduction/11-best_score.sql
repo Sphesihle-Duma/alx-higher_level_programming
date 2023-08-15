d9e671f9e85428fcc82250d3261f4f5e11ad4589
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-08-15 12:56:13 -0700

    added a script that displays the best scores

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- Lists all records with a score >= 10 from the second table
+-- Displays both score and name
+-- Records are ordered by score
+SELECT score, name
+FROM second_table
+WHERE score >= 10;
