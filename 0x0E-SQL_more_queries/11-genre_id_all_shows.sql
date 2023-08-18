b82732f2e027e969637dbb3b15b6fe7b4254b370
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-08-18 03:57:24 -0700

    Fixing the condition on the join

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- Lists all shows from the imported database
-SELECT shows.title, genre.genre_id
+SELECT  shows.title, genre.genre_id
 FROM tv_shows As shows
 LEFT JOIN tv_show_genres As genre
-ON shows.id = genre.genre_id
-ORDER BY shows.title ASC, genre.genre_id ASC
+ON shows.id = genre.show_id
+ORDER BY shows.title ASC, genre.genre_id
