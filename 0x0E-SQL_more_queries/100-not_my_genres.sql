234296ab01d0b1779855cd406e418b4de8643c7f
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-08-18 14:14:44 -0700

    Added a script that lists all genres that are not linked with a show

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,13 @@
+-- Lists all genres that are not linked to the show
+SELECT g.name
+FROM tv_genres as g
+LEFT JOIN(
+	SELECT * 
+	FROM tv_shows AS s
+	JOIN tv_show_genres AS tg
+	ON s.id = tg.show_id
+	WHERE s.title = "Dexter"
+) AS vt
+ON g.id = vt.genre_id
+WHERE vt.title IS NULL
+ORDER BY g.name;
