9b23648b0c979f56aa8bac1e4e516646af8d557f
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-09-16 07:41:22 -0700

    Added 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+    A script that searches an name
+    from the database. The name is
+    passed as an argument in the
+    script.
+    Args:
+       mysql username: user of the mysql
+       mysql password: password of the user
+       database name: the name of the database
+       state name: name of the state to search
+"""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    state_name = sys.argv[4]
+    pt = 3306
+    host = "localhost"
+    db_conn = MySQLdb.connect(host=host, user=user, passwd=passwd,
+                              db=db, port=pt)
+    cur = db_conn.cursor()
+    query = """SELECT * FROM states
+            WHERE name
+            LIKE %s
+            ORDER BY id ASC
+            """
+    cur.execute(query, (state_name, ))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db_conn.close()
