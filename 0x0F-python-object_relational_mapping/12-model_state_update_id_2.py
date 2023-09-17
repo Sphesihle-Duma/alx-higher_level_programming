8ab6089b1d4643bacbe9e03e48caf4e3c450aaab
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-09-17 12:26:42 -0700

    Added 12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Changes the name of a State object from the database hbtn_0e_6_usa
+Args:
+    mysql username: database user
+    mysql password: password of the user
+    database name: the name of the database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    db_conn = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                            pool_pre_ping=True)
+    Session = sessionmaker(bind=db_conn)
+    session = Session()
+
+    state_updated = session.query(State).filter(State.id == 2).first()
+
+    if state_updated:
+        state_updated.name = 'New Mexico'
+        session.commit()
+    session.close()
