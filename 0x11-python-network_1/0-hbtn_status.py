47455191f8b208751f7ce033ef4aad4a58379d15
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-09-29 13:45:07 -0700

    handling network issues

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """ Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
+import urllib.error  # Import the urllib.error module for error handling
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        res = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(res)))
-        print('\t- content: {}'.format(res))
-        print('\t- utf8 content: {}'.format(res.decode("utf-8")))
+    try:
+        with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
+            res = r.read()
+            print('Body response:')
+            print('\t- type: {}'.format(type(res)))
+            print('\t- content length: {}'.format(len(res)))
+            print('\t- utf8 content: {}'.format(res.decode("utf-8")))
+    except urllib.error.URLError as e:
+        print(f"Error: {e}")
