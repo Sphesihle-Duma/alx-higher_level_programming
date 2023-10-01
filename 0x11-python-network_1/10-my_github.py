18780a4667f041c2eabe45b96e496fb059f42d21
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-10-01 02:40:45 -0700

    Added 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """
-Python script that sends a search request to the
-Star Wars API, searching people
+Python script that takes GitHub credentials
+and uses the GitHub API to display the user id
 """
-import requests
+from requests import get, auth
 import sys
 
 
 if __name__ == "__main__":
-    search = 'https://swapi.co/api/people/?search={}'.format(sys.argv[1])
-    r = requests.get(search)
-    json_o = r.json()
-    print("Number of results: {}".format(json_o.get('count')))
-    for character in json_o.get('results'):
-        print(character.get('name'))
+    url = 'https://api.github.com/user'
+    user = sys.argv[1]
+    password = sys.argv[2]
+    r = get(url, auth=auth.HTTPBasicAuth(user, password))
+    print(r.json().get('id'))
